21a07eb168abdd20161b4ad414be43b87044a005
Devilmuffin <94016701+DevilMuffin@users.noreply.github.com>
2025-11-04 12:16:03 +0000

    base stuff

---
+++ main.py
@@ -0,0 +1,175 @@
+#Imports
+import random
+import sqlite3
+import sys
+
+#create database and tables if not created already
+try:
+    sqliteConnection = sqlite3.connect('ProjectData.db')
+    cursor = sqliteConnection.cursor()
+
+    query = """CREATE IF NOT EXISTS player (
+        id INTEGER AUTO_INCRAMENT PRIMARY KEY,
+        chipValue INTEGER NOT NULL,
+        wins INTEGER NOT NULL,
+        losses INTEGER NOT NULL,
+        betsPlaces INTEGER NOT NULL
+    );"""
+
+    cursor.execute(query)
+
+    sqliteConnection.commit()
+
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
+
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+
+#Create class for player (Update later)
+class Player:
+    def __init__(self, chipValue, wins, losses, betsPlaced):
+        self.__chipValue = chipValue
+        self.__wins = wins
+        self.__losses = losses
+        self.__betsPlaces = betsPlaced
+
+    def displayChips(self):
+        print(f'You have {self.__chipValue} chips')
+
+    def displayWins(self):
+        print(f'You have {self.__wins} wins')
+
+    def displayLosses(self):
+        print(f'You have {self.__losses} losses')
+
+    def displayWins(self):
+        print(f'You have placed {self.__betsPlaces} bets')
+
+
+#Setting card values
+cardValues = {i: str(i) for i in range(2, 11)}
+cardValues.update({
+    1: "Ace (Low)",
+    11: "Jack",
+    12: "Queen",
+    13: "King",
+    14: "Ace (High)"
+})
+
+
+#Creating suit dicts
+cardsHearts = {i: True for i in range(1, 14)}
+cardsDiamonds = {i: True for i in range(1, 14)}
+cardsSpades = {i: True for i in range(1, 14)}
+cardsClubs = {i: True for i in range(1, 14)}
+
+deck = {
+    "Hearts": cardsHearts,
+    "Diamonds": cardsDiamonds,
+    "Spades": cardsSpades,
+    "Clubs": cardsClubs
+}
+
+#Creating hand types
+
+
+def createCard(cardType, cardSuit):
+    cardSymbols = {i: str(i) for i in range(2, 11)}
+    cardSymbols.update({
+        1: "A",
+        11: "J",
+        12: "Q",
+        13: "K",
+    })
+
+    cardSuits = {
+        "Hearts": "♥",
+        "Diamonds": "♦",
+        "Clubs": "♣",
+        "Spades": "♠"
+    }
+
+    cardSymbol = cardSymbols[cardType]
+
+    cardSuitSymbol = cardSuits[cardSuit]
+
+    if cardType == 10:
+        card = f"""┌─────────┐
+│{cardSymbol}       │
+│         │
+│    {cardSuitSymbol}    │
+│         │
+│       {cardSymbol}│
+└─────────┘"""
+    else:
+        card = f"""┌─────────┐
+│{cardSymbol}        │
+│         │
+│    {cardSuitSymbol}    │
+│         │
+│        {cardSymbol}│
+└─────────┘"""
+
+    return card
+
+
+def generateCard():
+    while True:
+
+        cardSuit = random.choice(list(deck.keys()))
+        cardType = random.choice(list(deck[cardSuit].keys()))
+
+        if deck[cardSuit][cardType] != False:
+            card = createCard(cardType, cardSuit)
+            deck[cardSuit][cardType] = False
+            return card, cardSuit, cardType
+
+
+def dealCards():
+    card1, cardSuit1, cardType1 = generateCard()
+    card2, cardSuit2, cardType2 = generateCard()
+    card3, cardSuit3, cardType3 = generateCard()
+    cards = [card1, card2, card3]
+    splitCards = [card.split("\n") for card in cards]
+
+    for lines in zip(*splitCards):
+        print(" ".join(lines))
+
+#Play Game
+def playGame():
+    print("In this game there will be 6 players including yourself")
+    print("We will now begin the game")
+    
+
+#View Stats
+def viewStats():
+    pass
+
+#Exit
+def endProgram():
+    sys.exit()
+
+def startProgram():
+    print("Welcome to terminal poker")
+    print("Please select what you would like to do")
+    print("1: Play")
+    print("2: View Player Stats")
+    print("3: Exit")
+    choice = int
+    while choice not in [1, 2, 3]:
+        try:
+            choice = int(input("Enter Choice: "))
+        except:
+            print("Please enter a correct option")
+    if choice == 1:
+        playGame()
+    elif choice == 2:
+        viewStats()
+    elif choice == 3:
+        endProgram()
+    
+
+#Running the program
+startProgram()
